8b5eec2aec1ac5d07c65c0b52d20808ce94ccb88
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-04-14 02:56:35 +0200

    inicio

---
+++ app/src/main/java/com/javidev/matrix/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.javidev.matrix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.javidev.matrix.ui.MatrixRain
+import com.javidev.matrix.ui.theme.MatrixTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MatrixTheme {
+                Box(modifier = Modifier.fillMaxSize().background(Color.Black).padding(2.dp)){
+                    MatrixRain()
+                }
+
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/javidev/matrix/ui/MatrixRain.kt
@@ -0,0 +1,99 @@
+package com.javidev.matrix.ui
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.javidev.matrix.model.charters
+import com.javidev.matrix.model.charters2
+import com.javidev.matrix.model.charters3
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun MatrixRain(rowRepeat: Int= 20) {
+    
+    Row {
+       repeat(rowRepeat){
+           MatrixColum(
+               crawlSpeed = (100L..500L).random(),
+               starDelay = (100L..2000L).random()
+           )
+       }
+    }
+
+}
+
+@Composable
+fun RowScope.MatrixColum(crawlSpeed: Long,starDelay: Long) {
+
+    BoxWithConstraints(Modifier.weight(1f).fillMaxHeight() ) {
+        // como el char cambia con el alpha esto hace que el composable
+        // se reconponga para evitarlo uso un remember
+        val newChar = remember { mutableListOf(charters2.reversed(),charters, charters2,charters2.shuffled()).random()}.toMutableList()
+        var letrasToDrawer by remember { mutableStateOf(0)}
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            repeat(letrasToDrawer){
+                MatrixChar(
+                    char = if (letrasToDrawer < newChar.size * 0.7) newChar[it] else charters3[(1..19).random()],
+                    crawlSpeed = crawlSpeed,
+                    // si la columna llega al 70% reinicio la columna a 0 y empieza a bajar otra vez
+                    finishListener = { if (it >= newChar.size * 0.6) letrasToDrawer = 0}
+                )
+            }
+        }
+
+        LaunchedEffect(Unit){
+            // este delay es para que tarde un tiempo en pintar al inicio de la letra
+            delay(starDelay)
+            while (true){
+                if (letrasToDrawer < newChar.size) letrasToDrawer += 1
+                delay(crawlSpeed)
+
+            }
+        }
+
+    }
+
+
+
+
+}
+
+@Composable
+fun MatrixChar(char: String,crawlSpeed: Long,finishListener: ()-> Unit) {
+    var texColor by remember{ mutableStateOf(Color(0xffcefbe4))}
+    var starFade by remember { mutableStateOf(false)}
+    val alpha by animateFloatAsState(
+        targetValue = if (starFade) 0f else 1f, // cambio el color
+        animationSpec = tween( durationMillis = 5000, easing = LinearEasing),
+        // este parametro nos dice cuando la animacion llego al final
+        // yo lo uso para que al llegar al 70% reinicio la letra
+        finishedListener = {finishListener()}
+    )
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(30.dp)) {
+        Text(
+            text = char,
+            color = texColor.copy(alpha = alpha),
+            fontSize = 20.sp,
+        )
+    }
+
+    // para lanzar la animacion cambio alpha y lo hago solo una vez
+    // y lo consigo con Unit tambien cambio el color y le doy un delay para
+    // que primero salga el color por defecto que es el blanco
+    LaunchedEffect(Unit){
+        delay(crawlSpeed)
+        texColor = Color(0xff43c748)
+        starFade = true
+    }
+}
